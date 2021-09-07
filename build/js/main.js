61dc6ae78579938d62ee3dacf742869569389e9d
Valery Alikin <weblcw@gmail.com>
2021-09-07 18:54:37 +0500

    Initial commit

---
+++ build/js/main.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var swiper = new Swiper('.nav', {
+  spaceBetween: 3,
+  slidesPerView: 'auto'
+});
+var swiper2 = new Swiper('.main', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  thumbs: {
+    swiper: swiper
+  }
+});
+
+var mobileMenu = function mobileMenu() {
+  if ($(window).width() <= 767 && $('.hamburger').length === 0) {
+    $('.header .header__right').prepend($('.header__nav'));
+    $('.header .wrap > .clearfix').append('<div class="hamburger"><span></span></div>');
+  } else if ($(window).width() >= 768) {
+    $('.hamburger').remove();
+    $('.header .wrap').append($('.header__nav'));
+  }
+};
+
+mobileMenu();
+$(window).on('resize', mobileMenu);
+$('body').on('click', '.hamburger', function (e) {
+  $(e.currentTarget).toggleClass('active');
+  $('.header__right').toggleClass('active');
+});
